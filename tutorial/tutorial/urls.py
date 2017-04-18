fbdab954a4ec4cb22c731ef7a707859a76b9ea9d
aiirohituzi <sinn5656@gmail.com>
2017-04-18 23:04:39 +0900

    drf tutorial-requests and responses

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
